28ca3423ee810eaf1b8e4d12e70b543d2486bed0
Oğuzhan Nalbant <oguz.nalbant@gmail.com>
2023-07-29 17:23:28 +0300

    created repository interface docker compose init

---
+++ Brand.AdvertManagement/Repository/AdvertRepository.cs
@@ -0,0 +1,23 @@
+﻿using Brand.AdvertManagement.Model.DTO.Request;
+using Brand.AdvertManagement.Model.DTO.Response;
+
+namespace Brand.AdvertManagement.Repository
+{
+    public class AdvertRepository : IAdvertRepository
+    {
+        public async Task<AdvertResponseDto> GetAdvert(GetAdvertRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AdvertListResponseDto> GetAdvertList(GetAdvertListByFilterRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task VisitAdvert(VisitAdvertRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Brand.AdvertManagement/Repository/IAdvertRepository.cs
@@ -0,0 +1,14 @@
+﻿using Brand.AdvertManagement.Model.DTO.Request;
+using Brand.AdvertManagement.Model.DTO.Response;
+
+namespace Brand.AdvertManagement.Repository
+{
+    public interface IAdvertRepository
+    {
+        Task<AdvertResponseDto> GetAdvert(GetAdvertRequestDto requestDto);
+
+        Task<AdvertListResponseDto> GetAdvertList(GetAdvertListByFilterRequestDto requestDto);
+
+        Task VisitAdvert(VisitAdvertRequestDto requestDto);
+    }
+}
